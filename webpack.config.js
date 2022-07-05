271f90d0ee07599d388d433d8ec7b4ebad8680eb
Sebastian <s.romhany@gmail.com>
2022-07-05 16:21:49 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const { watch } = require("fs");
+const path = require("path");
+
+module.exports = {
+   entry: './src/index.js',
+   output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, 'dist'),
+   },
+   mode: "development",
+   module: {
+      rules: [{
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+        {
+         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+         use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+              }
+            }
+          ]
+  
+        }
+      ],
+   },
+   watch: true,
+}
